0472b89ab3302a27b5cee31e21b6f3caa522e3cb
rsandhumcr <rsandhumcr@gmail.com>
2025-06-15 15:05:10 +0100

    Add logging and parameter log utiltites.

---
+++ src/CompanyNameSpace.ProjectName.Application/Utils/LocalLogger.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+
+namespace CompanyNameSpace.ProjectName.Application.Utils;
+public class LocalLogger
+{
+    public static void ExceptionLogger(Exception ex, string message, string filename)
+    {
+        var exceptionMessage = FormatException(ex);
+        var wholeMessage = CreateMessageHeader(message, exceptionMessage);
+        WriteStringToFile(filename, wholeMessage);
+    }
+
+    public static void MessageLogger(string message, string filename)
+    {
+        var wholeMessage = CreateMessageHeader(message, string.Empty);
+        WriteStringToFile(filename, wholeMessage);
+    }
+
+    public static async Task ExceptionLoggerAsync(Exception ex, string message, string filename)
+    {
+        var exceptionMessage = FormatException(ex);
+        var wholeMessage = CreateMessageHeader(message, exceptionMessage);
+        await WriteStringToFileAsync(filename, wholeMessage);
+    }
+
+
+    public static async Task MessageLoggerAsync(string message, string filename)
+    {
+        var wholeMessage = CreateMessageHeader(message, string.Empty);
+        await WriteStringToFileAsync(filename, wholeMessage);
+    }
+
+    private static string CreateMessageHeader(string message01, string message02)
+    {
+        var now = DateTime.Now;
+        var sb = new StringBuilder($"--- {now:yyyy-MM-dd HH:mm:ss}\n");
+        sb.AppendLine($"msg : {message01}");
+        if (!string.IsNullOrWhiteSpace(message02))
+            sb.AppendLine(message02);
+        return sb.ToString();
+    }
+
+    public static string FormatException(Exception ex, int level = 0)
+    {
+        if (ex == null)
+            return string.Empty;
+        var sb = new StringBuilder();
+        sb.AppendLine(string.Empty);
+        sb.AppendLine($"level {level}");
+        sb.AppendLine("==========");
+        sb.AppendLine($"Message: {ex.Message}");
+        sb.AppendLine($"Source: {ex.Source}");
+        sb.AppendLine($"Stack: {ex.StackTrace}");
+        if (ex.InnerException != null)
+            sb.AppendLine(FormatException(ex.InnerException, level + 1));
+        return sb.ToString();
+    }
+
+    private static void WriteStringToFile(string filename, string textData)
+    {
+        var fullPath = GetFullPath(filename);
+        if (File.Exists(fullPath))
+            File.AppendAllText(fullPath, textData);
+        else
+            File.WriteAllText(fullPath, textData);
+    }
+
+    private static async Task WriteStringToFileAsync(string filename, string textData)
+    {
+        var fullPath = GetFullPath(filename);
+        if (File.Exists(fullPath))
+            await File.AppendAllTextAsync(fullPath, textData);
+        else
+            await File.WriteAllTextAsync(fullPath, textData);
+    }
+
+    private static string GetFullPath(string filename)
+    {
+        if (string.IsNullOrEmpty(filename))
+            filename = "UserAppExceptionLog.txt";
+        return Path.Combine(Path.GetTempPath(), filename);
+    }
+
+    public static void ExceptionLogger(Exception ex, string parameterData, string message, string filename)
+    {
+        var exceptionMessage = FormatException(ex);
+        var wholeMessage = CreateMessageHeader(message + "\n" + parameterData, exceptionMessage);
+        WriteStringToFile(filename, wholeMessage);
+    }
+
+    public static async Task ExceptionLoggerAsync(Exception ex, string parameterData, string message, string filename)
+    {
+        var exceptionMessage = FormatException(ex);
+        var wholeMessage = CreateMessageHeader(message + "\n" + parameterData, exceptionMessage);
+        await WriteStringToFileAsync(filename, wholeMessage);
+    }
+}
---
+++ src/CompanyNameSpace.ProjectName.Application/Utils/MethodParameterLogUtility.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace CompanyNameSpace.ProjectName.Application.Utils;
+
+//var parameterData = new MethodParameterLogUtility(() => parameterValue).GetParameterValues();
+//var parameterData = new MethodParameterLogUtility(() => parameterOne, () => parameterTwo).GetParameterValues();
+
+public class MethodParameterLogUtility
+{
+    private string _methodName;
+    private string _paramaterLog;
+
+    private readonly Dictionary<string, Type> _methodParamaters;
+    private readonly List<Tuple<string, Type, object>> _providedParametars;
+
+    public MethodParameterLogUtility(params Expression<Func<object>>[] providedParameters)
+    {
+        try
+        {
+            var currentMethod = new StackTrace().GetFrame(1).GetMethod();
+
+            /*Set class and current method info*/
+            _methodName = String.Format("Class = {0}, Method = {1}", currentMethod.DeclaringType.FullName, currentMethod.Name);
+
+            /*Get current methods parameters*/
+            _methodParamaters = new Dictionary<string, Type>();
+            (from aParamater in currentMethod.GetParameters()
+             select new { Name = aParamater.Name, DataType = aParamater.ParameterType })
+             .ToList()
+             .ForEach(obj => _methodParamaters.Add(obj.Name, obj.DataType));
+
+            /*Get provided methods parameters*/
+            _providedParametars = new List<Tuple<string, Type, object>>();
+            foreach (var aExpression in providedParameters)
+            {
+                Expression bodyType = aExpression.Body;
+
+                if (bodyType is MemberExpression)
+                {
+                    AddProvidedParamaterDetail((MemberExpression)aExpression.Body);
+                }
+                else if (bodyType is UnaryExpression)
+                {
+                    UnaryExpression unaryExpression = (UnaryExpression)aExpression.Body;
+                    AddProvidedParamaterDetail((MemberExpression)unaryExpression.Operand);
+                }
+                else
+                {
+                    throw new Exception("Expression type unknown.");
+                }
+            }
+
+            /*Process log for all method parameters*/
+            ProcessLog();
+
+        }
+        catch (Exception exception)
+        {
+            throw new Exception("Error in paramater log processing.", exception);
+        }
+    }
+
+    private void ProcessLog()
+    {
+        try
+        {
+            foreach (var methodParameter in _methodParamaters)
+            {
+                var aParameter =
+                    _providedParametars.Where(
+                        obj => obj.Item1.Equals(methodParameter.Key) && obj.Item2 == methodParameter.Value).Single();
+                _paramaterLog += String.Format(@" ""{0}"":{1},", aParameter.Item1, JsonConvert.SerializeObject(aParameter.Item3));
+            }
+            _paramaterLog = (_paramaterLog != null) ? _paramaterLog.Trim(' ', ',') : string.Empty;
+        }
+        catch (Exception exception)
+        {
+            throw new Exception($"Method Paramater is not found in providedParameters. {exception.Message}");
+        }
+    }
+
+    private void AddProvidedParamaterDetail(MemberExpression memberExpression)
+    {
+        ConstantExpression constantExpression = (ConstantExpression)memberExpression.Expression;
+        var name = memberExpression.Member.Name;
+        var value = ((FieldInfo)memberExpression.Member).GetValue(constantExpression?.Value);
+        var type = value?.GetType();
+        _providedParametars.Add(new Tuple<string, Type, object>(name, type, value));
+    }
+
+
+    public string GetParameterValues()
+    {
+        var parameterValues = $"{_methodName}({_paramaterLog})";
+        return parameterValues
+            .Replace(",\"", ", \"")
+            .Replace("\":{", "\": {");
+    }
+
+}
